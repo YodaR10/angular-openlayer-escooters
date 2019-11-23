dae041e5b2c93a146a6eb8767dc317d491932196
YodaR10 <dellag.dario@gmail.com>
2019-11-23 19:06:13 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
 
-import { Map, View } from 'ol';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
-import { XYZ, Vector as VectorSource } from 'ol/source';
-import { Style, Fill, Stroke, Circle } from 'ol/style';
-import { GeoJSON } from 'ol/format';
-import { click, singleClick } from 'ol/events/condition';
-import Select from 'ol/interaction/Select';
-import OSM, {ATTRIBUTION} from 'ol/source/OSM';
+import { Map, View } from "ol";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { XYZ, Vector as VectorSource } from "ol/source";
+import { Style, Fill, Stroke, Circle } from "ol/style";
+import { GeoJSON } from "ol/format";
+import { click, singleClick } from "ol/events/condition";
+import Select from "ol/interaction/Select";
+import OSM, { ATTRIBUTION } from "ol/source/OSM";
+import KML from "ol/format/KML";
 
 // rayon 2*PI*r
 const radius = 50;
 const perimeter = 2 * Math.PI * radius;
-const lineCap = 'but';
+const lineCap = "but";
 const width = 8;
 
 const repart: { nb: number; color: string }[] = [
- { nb: 6, color: 'red' },
- { nb: 24, color: 'green' },
- { nb: 5, color: 'orange' },
- { nb: 12, color: 'purple' },
+  { nb: 6, color: "red" },
+  { nb: 24, color: "green" },
+  { nb: 5, color: "orange" },
+  { nb: 12, color: "purple" }
 ];
 
-function getDonutStyles(radius: number, width: number, repart: { nb: number; color: string }[]) {
+function getDonutStyles(
+  radius: number,
+  width: number,
+  repart: { nb: number; color: string }[]
+) {
   const perimeter = 2 * Math.PI * radius;
   const total = repart.reduce((acc, curr) => acc + curr.nb, 0);
   const styles = [];
@@ -38,8 +43,13 @@ function getDonutStyles(radius: number, width: number, repart: { nb: number; col
             stroke: new Stroke({
               color: curr.color,
               width,
-              lineDash: [0, acc === 0 ? 0 : perimeter * acc / total, perimeter * curr.nb / total,  perimeter],
-              lineCap: 'but'
+              lineDash: [
+                0,
+                acc === 0 ? 0 : (perimeter * acc) / total,
+                (perimeter * curr.nb) / total,
+                perimeter
+              ],
+              lineCap: "but"
             })
           })
         })
@@ -51,17 +61,17 @@ function getDonutStyles(radius: number, width: number, repart: { nb: number; col
 }
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent  {
+export class AppComponent {
   map: Map;
   vectorSource: VectorSource = new VectorSource();
-  
+
   ngOnInit() {
     this.map = new Map({
-      target: 'map',
+      target: "map",
       layers: [
         /*new TileLayer({
           source: new XYZ({ url: 'https://{a-c}.tile.osm.org/{z}/{x}/{y}.png' })
@@ -79,8 +89,18 @@ export class AppComponent  {
               'All maps © <a href="https://www.opencyclemap.org/">OpenCycleMap</a>',
               ATTRIBUTION
             ],
-            url: 'https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png' +
-                '?apikey=e40eafce191d45d9996738a6926e8e16'
+            url:
+              "https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png" +
+              "?apikey=e40eafce191d45d9996738a6926e8e16"
+          })
+        }),
+        new VectorLayer({
+          source: new VectorSource({
+            url: "./kml/ComuniInteressati.kml",
+            format: new KML({
+                    extractStyles: true,
+                    extractAttributes: true
+                }),
           })
         })
       ],
@@ -90,26 +110,25 @@ export class AppComponent  {
       })
     });
 
-    const geojson =  {
-      "type": "FeatureCollection",
-      "features": [
+    const geojson = {
+      type: "FeatureCollection",
+      features: [
         {
-          "type": "Feature",
-          "properties": {},
-          "geometry": {
-            "type": "Point",
-            "coordinates": [
-              3.115096092224121,
-              45.78757682111226
-            ]
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "Point",
+            coordinates: [3.115096092224121, 45.78757682111226]
           }
         }
       ]
     };
 
-    this.vectorSource.addFeatures((new GeoJSON()).readFeatures(geojson, {
-      dataProjection : 'EPSG:4326',
-      featureProjection: 'EPSG:3857'
-    })); 
+    this.vectorSource.addFeatures(
+      new GeoJSON().readFeatures(geojson, {
+        dataProjection: "EPSG:4326",
+        featureProjection: "EPSG:3857"
+      })
+    );
   }
 }
